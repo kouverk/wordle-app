cdf442c08ff3a24f43bea1b85cd75eaa2ab7417b
Kouver Bingham <kouverk@gmail.com>
2024-10-02 21:46:37 -0600

    Minor changes to app.config.ts

--- wordleapp/src/app/app.config.ts
+++ wordleapp/src/app/app.config.ts
@@ -1,5 +1,7 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+import { withInterceptors } from '@angular/common/http';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
@@ -8,8 +10,8 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes), 
     provideClientHydration(),
-    provideHttpClient(
-      withInterceptors([JwtInterceptor])
-    ),
+    // provideHttpClient(
+    //   withInterceptors([JwtInterceptor])
+    // ),
 ]
 };
