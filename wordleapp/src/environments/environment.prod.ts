5a23e2bc6ba5c2a01c4898a8fae2dd62a9f4411d
Kouver Bingham <kouverk@gmail.com>
2025-11-29 13:02:31 -0700

    Update production API URL to Railway backend

--- wordleapp/src/environments/environment.prod.ts
+++ wordleapp/src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  // This will be replaced with your Railway backend URL after deployment
-  apiUrl: 'https://YOUR_RAILWAY_BACKEND_URL'
+  apiUrl: 'https://charming-insight-production.up.railway.app'
 };
